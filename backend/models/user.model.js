6642d77ed4356329fcaca73563e067b791350640
Cenote <himangshukalita131@gmail.com>
2024-07-23 14:30:30 +0530

    User model

---
+++ backend/models/user.model.js
@@ -0,0 +1,56 @@
+import mongoose from "mongoose"
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+
+    LastName: {
+      type: String,
+      required: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+
+    profileImagePath: {
+      type: String,
+      default: "",
+    },
+
+    tripList: {
+      type: Array,
+      default: [],
+    },
+
+    wishList: {
+      type: Array,
+      default: [],
+    },
+
+    propertyList: {
+      type: Array,
+      default: [],
+    },
+
+    reservationList: {
+      type: Array,
+      default: [],
+    },
+  },
+  { timestamps: true }
+)
+
+const User = mongoose.model("User", userSchema)
+
+export default User
