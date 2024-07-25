a70f6617d389c938d4be76767cd611d80571633e
Cenote <himangshukalita131@gmail.com>
2024-07-26 02:47:12 +0530

    Add listing to wishlist api route

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -1,4 +1,6 @@
 import Booking from "../models/booking.model.js"
+import Listing from "../models/listing.model.js"
+import User from "../models/user.model.js"
 import { errorHandler } from "../utils/error.js"
 
 export const getTripList = async (req, res, next) => {
@@ -14,6 +16,52 @@ export const getTripList = async (req, res, next) => {
     }
 
     res.status(200).json(trips)
+  } catch (error) {
+    next(error)
+  }
+}
+
+export const addListingToWishList = async (req, res, next) => {
+  try {
+    const { userId, listingId } = req.params
+
+    const user = await User.findById(userId)
+
+    if (!user) {
+      return next(errorHandler(404, "User not found!"))
+    }
+
+    const listing = await Listing.findById(listingId).populate("creator")
+
+    if (!listing) {
+      return next(errorHandler(404, "Listing not found!"))
+    }
+
+    const favoriteListing = user.wishList.find(
+      (item) => item._id.toString() === listingId
+    )
+
+    if (favoriteListing) {
+      user.wishList = user.wishList.filter(
+        (item) => item._id.toString() !== listingId
+      )
+
+      await user.save()
+
+      res.status(200).json({
+        message: "Listing is removed from wishlist",
+        wishList: user.wishList,
+      })
+    } else {
+      user.wishList.push(listing)
+
+      await user.save()
+
+      res.status(200).json({
+        message: "Listing is added to wishlist",
+        wishList: user.wishList,
+      })
+    }
   } catch (error) {
     next(error)
   }
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,8 +1,13 @@
 import express from "express"
-import { getTripList } from "../controller/user.controller.js"
+import {
+  addListingToWishList,
+  getTripList,
+} from "../controller/user.controller.js"
 
 const router = express.Router()
 
 router.get("/:userId/trips", getTripList)
+
+router.patch("/:userId/:listingId", addListingToWishList)
 
 export default router
